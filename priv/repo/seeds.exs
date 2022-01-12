a2e452589195063bdf0615fa19dbea799243dd0d
Michail Karavaev <michail.karavaev@taxfix.de>
2022-01-12 16:40:31 +0100

    Add users table seed

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,19 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+inserted_at = updated_at = NaiveDateTime.utc_now() |> NaiveDateTime.truncate(:second) 
+
+
+chunks =
+  List.duplicate(%{inserted_at: inserted_at, updated_at: updated_at, points: 0}, 1_000_000)
+  |> Enum.chunk_every(21845)
+
+
+Foo.Repo.transaction(fn ->
+  Task.async_stream(chunks, fn chunk ->
+    {count, _} = Foo.Repo.insert_all(Foo.User, chunk)
+    count
+  end, ordered: false)
+  |> Enum.reduce(0, fn {:ok, n}, acc -> acc + n end)
+end)
