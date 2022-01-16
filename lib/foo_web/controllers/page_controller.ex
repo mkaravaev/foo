5b703f6a4976699bc82a2810f2551f3520409203
Michail Karavaev <michail.karavaev@taxfix.de>
2022-01-16 17:34:47 +0100

    Increase timout when calling generator

--- lib/foo_web/controllers/page_controller.ex
+++ lib/foo_web/controllers/page_controller.ex
@@ -1,9 +1,12 @@
 defmodule FooWeb.PageController do
   use FooWeb, :controller
+
   alias Foo.Generator
 
+  @timeout :timer.seconds(10)
+
   def index(conn, _params) do
-    with {:ok, resp} <- Generator.fetch() do
+    with {:ok, resp} <- Generator.fetch(@timeout) do
       render(conn, "index.json", resp)
     else
       _error -> {:error, "Can't process request"}
